5d2b7c887af8a7f30c867bb63f84b1e11251af11
aziddy <alexander.zidros@gmail.com>
2019-03-13 00:48:52 -0400

    finally got torrents streaming in with sync controls

--- movieServer.js
+++ movieServer.js
@@ -6,6 +6,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var base64 = require('base-64');
 var utf8 = require('utf8');
+var Transcoder = require('stream-transcoder');
+
 
 var connectedUsers = new Array();
 var allUserLatency = new Array();
@@ -15,12 +17,15 @@ app.set('view engine', 'ejs');
 // server-client-server RTT
 var timeThing;
 var startTime;
-
+var userTorrentStreams = {};
 
 var torrentStream = require('torrent-stream');
- 
+ /*
 var engine = torrentStream('magnet:?xt=urn:btih:f73a9012655bc2b4f33f4de98965e9fcfd1f27ab&dn=Riverdale.US.S03E14.HDTV.x264-SVA%5Bettv%5D&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969');
 var streamy;
+
+
+
  
  
 engine.on('ready', function() {
@@ -31,7 +36,7 @@ engine.on('ready', function() {
 	
 
 });
-
+*/
 //var engine;
 
 
@@ -59,7 +64,7 @@ app.get('/:magnet', function(req, res) {
 
 
 // Actual Video Player
-app.get('/videoPlayer',function(req,res){
+app.get('/videoPlayer/:base64_magnet',function(req,res){
 	res.sendFile(__dirname + '/videoPlayer.html');
 });
 
@@ -71,12 +76,6 @@ app.get('/enterMagnet',function(req,res){
 app.get('/selectVideoFile/:magnet',function(req,res){
 	
 	var decodedB64 = base64.decode(req.params.magnet);
-	
-	
-	
-	
-	
-	
 	var engine2 = torrentStream(decodedB64);
  
 	engine2.on('ready', function() {
@@ -102,11 +101,12 @@ app.get('/selectVideoFile/:magnet',function(req,res){
 });
 
 
-app.get('/videoPlayer2',function(req,res){
+app.get('/videoPlayer2/:base64_magnet',function(req,res){
 	
 	
 	var data = {
-		magnet: base64.encode('yeet'),
+		//magnet: base64.encode(req.params.base64_magnet),
+		magnet: req.params.base64_magnet
 	};
 	
 	res.render('videoPlayer2', data);
@@ -123,6 +123,21 @@ app.get('/videoDirectory',function(req,res){
 	
 });
 
+var torrentStreamOpt = {
+	
+	connections: 500,     // Max amount of peers to be connected to.
+	uploads: 500,          // Number of upload slots.
+	dht: true,            // Whether or not to use DHT to initialize the swarm.
+	                      // Defaults to true
+	tracker: true,        // Whether or not to use trackers from torrent file or magnet link
+	                      // Defaults to true
+	trackers: [
+	    'udp://tracker.openbittorrent.com:80',
+	    'udp://tracker.ccc.de:80'
+	]
+	                      // Allows to declare additional custom trackers to use
+	                      // Defaults to empty
+};
 
 // Handles Video Streaming
 /*
@@ -160,8 +175,15 @@ app.get('/videoStream/:videoFileName', function(req, res) {
     }
 });
 */
-app.get('/videoStream/:videoFileName', function(req, res) {
 
+
+
+/*
+
+
+app.get('/videoStream/:socketID/:videoFileName', function(req, res) {
+
+	var socketID = req.params.socketID;
 
     const fileSize = engine.files[1].length;
     const range = req.headers.range
@@ -198,6 +220,73 @@ app.get('/videoStream/:videoFileName', function(req, res) {
 
 
 
+*/
+
+
+
+app.get('/videoStream/:socketID/:videoFileName', function(req, res) {
+
+	var socketID = req.params.socketID;
+	
+	
+	var biggestFile = userTorrentStreams[socketID].files[0];
+	
+	
+	// Stream Biggest File (inefficient CHECK)
+	for(var x = 0; x < userTorrentStreams[socketID].files.length; x++){
+		
+		console.log(userTorrentStreams[socketID].files[x].name);
+		
+		if(biggestFile.length < userTorrentStreams[socketID].files[x].length){
+			biggestFile = userTorrentStreams[socketID].files[x];
+		}
+	}
+	
+	
+	
+
+	//if(userTorrentStreams.includes(socketID)){
+		var fileSize;
+		try{
+			fileSize = biggestFile.length;
+		} catch(err){
+			console.log(err);
+		}
+		const range = req.headers.range
+		if (range) {
+		    const parts = range.replace(/bytes=/, "").split("-")
+		    const start = parseInt(parts[0], 10)
+		    const end = parts[1] ?
+		        parseInt(parts[1], 10) :
+		        fileSize - 1
+		    const chunksize = (end - start) + 1
+		    const file = biggestFile.createReadStream({
+		        start,
+		        end
+		    })
+		    const head = {
+		        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+		        'Accept-Ranges': 'bytes',
+		        'Content-Length': chunksize,
+		        'Content-Type': 'video/mp4',
+		    }
+		    res.writeHead(206, head);
+		    file.pipe(res);
+		} else {
+		    const head = {
+		        'Content-Length': fileSize,
+		        'Content-Type': 'video/mp4',
+		    }
+		    res.writeHead(200, head)
+		    	biggestFile.createReadStream().pipe(res)
+		}
+	//}
+});
+
+
+
+
+
 io.on('connection', function(socket) {
 	
 	
@@ -226,9 +315,34 @@ io.on('connection', function(socket) {
 				allUserLatency.splice(x, 1);
 			}
 		}
+		
+		gcTorrentStreams();
+		
 		io.emit('connectedUsers', {
 			users: Object.keys(io.sockets.sockets)
 		});
+	});
+
+
+
+	// Torrent
+
+	socket.on('updateTorrentStream', function(msg) {
+		console.log("TORRENT--------");
+
+		userTorrentStreams[socket.id] = torrentStream(msg,torrentStreamOpt);
+		gcTorrentStreams();
+		
+		//socket.emit('torrentStreamReady', true);
+		userTorrentStreams[socket.id].on('ready', function() {
+	
+			//console.log('filename:', engine.files[1].name);
+			//console.log('filename:', engine.files[1].length);	
+			console.log("TORRENT READY");
+			socket.emit('torrentStreamReady', true);		
+		});
+		//userTorrentStreams.push()
+		//io.emit('updateCurrentVideoTime', msg);
 	});
 
 
@@ -247,6 +361,7 @@ io.on('connection', function(socket) {
 	socket.on('setCurrentVideoPause', function(msg) {
 		io.emit('updateCurrentVideoPause', msg);
 	});
+
 
 
 
@@ -303,6 +418,55 @@ io.on('connection', function(socket) {
 
 
 });
+
+
+
+function gcTorrentStreams(){
+		
+	var userTorrentStreamsKeyArray = Object.keys(userTorrentStreams)
+	var socketIDarray = Object.keys(io.sockets.sockets)
+	
+	
+	//console.log("TORRENT STREAM KEYS----------");
+	for(var x = 0; x < userTorrentStreamsKeyArray.length; x++){
+		
+		var doesntExist = true;
+		
+		for(var y = 0; y < socketIDarray.length; y++){
+			if(userTorrentStreamsKeyArray[x] == socketIDarray[y]){
+				doesntExist = false;
+			}
+		}
+		
+		if(doesntExist){
+			delete userTorrentStreams[userTorrentStreamsKeyArray[x]];
+		}
+		//console.log(userTorrentStreamsKeyArray[x]);
+	}
+	
+	
+	
+	userTorrentStreamsKeyArray = Object.keys(userTorrentStreams);
+	
+	
+	
+	console.log("SOCKET KEYS----------");
+	
+	for(var x = 0; x < socketIDarray.length; x++){
+		
+		console.log(socketIDarray[x]);
+		//console.log(userTorrentStreams[socketIDarray[x]]);
+	}
+	
+	console.log("TORRENT STREAM KEYS----------");
+	for(var x = 0; x < userTorrentStreamsKeyArray.length; x++){
+		console.log(userTorrentStreamsKeyArray[x]);
+	}
+}
+
+
+
+
 
 http.listen(2999, function() {
 	console.log('listening on *:2999');
